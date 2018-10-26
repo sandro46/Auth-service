dc6d0cb22b01ee029c42571e7615bf7626e34578
sandro46 <sandro4692@yandex.ru>
2018-10-27 01:27:18 +0300

    Up1

--- config.py
+++
@@ -1,7 +0,0 @@
-class Configurator(object):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'b31b7596'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://web:qwerty121314Q@62.109.16.96/project'
-    secret = 'b31b7596'
-    TEST = True
